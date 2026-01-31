45a2f373d93dd9296367718d07d3fc5962cc22f0
Revannath Khodade <revannathkhodade01@gmail.com>
2026-01-31 22:25:03 +0530

    Update StudentController.java

--- src/main/java/net/javaguides/sms/controller/StudentController.java
+++ src/main/java/net/javaguides/sms/controller/StudentController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import net.javaguides.sms.entity.Student;
 import net.javaguides.sms.service.StudentService;
 
